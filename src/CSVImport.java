ab2e496229d935bc836b70367289b46b5309f5ec
Michael Sasen Davidsen <md.vidsen@gmail.com>
2026-03-25 17:40:35 +0100

    Updated CSVImport

--- src/CSVImport.java
+++ src/CSVImport.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CSVImport {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         fileReader();
     }
 
